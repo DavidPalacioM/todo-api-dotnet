2195643d2fdca22f59c5562350a40cc4ad242678
DaviPalacio <david22pm@gmail.com>
2025-06-25 17:38:25 +0200

    Habilitar Swagger en producción

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddSingleton<TareaService>();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment() )
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction() )
 {
     app.UseSwagger();
     app.UseSwaggerUI();
